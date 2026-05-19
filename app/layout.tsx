84a793e18a2003aa40b61814fa0699e7ba3fda36
Yut8853 <hello@junkbranding.com>
2026-05-19 15:14:32 +0900

    META修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const _geistMono = Geist_Mono({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Creative Dev Blog',
-  description: 'CodePenブログのNext.js版テンプレート',
+  description: 'JUNKBRANDINGのクリエイティブテックブログ',
   icons: {
     icon: [
       {
